a9b4dbebb44725a0f961fc5da7abb1ddf9c00706
Marcin Małkus <malkus.marcin@gmail.com>
2022-08-03 23:53:00 +0200

    ex1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+	.map(
+		(item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+	)
+	.join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+gallery.addEventListener("click", (event) => {
+	event.preventDefault();
+	if (event.target.nodeName !== "IMG") return;
+	const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`);
+	instance.show();
+	const visible = instance.visible();
+	if (visible) {
+		document.addEventListener("keydown", (event) => {
+			if (event.key === "Escape") {
+				instance.close();
+			}
+		});
+	}
+});
